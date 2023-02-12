174571577de7ca23b9090b9ee6cc30512b2240e4
kyletaylor94 <kyletaylorrules94@gmail.com>
2023-02-12 22:41:52 +0100

    reminder4

--- DrunkenMaster/ViewController.swift
+++ DrunkenMaster/ViewController.swift
@@ -8,27 +8,84 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UNUserNotificationCenterDelegate {
     
     let notifications = UNUserNotificationCenter.current()
 
+    
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var messageTextField: UITextField!
     
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    @IBOutlet weak var okButtonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        okButtonLabel.isHidden = true
         // Do any additional setup after loading the view.
         notifications.requestAuthorization(options: [.alert, .sound]) { (permissionGranted, error) in
             if (!permissionGranted){
                 print("Permission Denied")
             }
         }
+        notifications.delegate = self
     }
-
+    
     @IBAction func buttonAction(_ sender: Any) {
+        first()
+        second()
+    }
+    
+    
+    
+    func formattedDate(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMM y HH:mm"
+        return formatter.string(from: date)
+    }
+    
+    
+    func first() {
+        let notifications = UNUserNotificationCenter.current()
+        
+        notifications.getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                
+                let title = self.titleTextField.text!
+                let message = self.messageTextField.text!
+                let date = self.datePicker.date
+                
+                if (settings.authorizationStatus == .authorized){
+                    let content = UNMutableNotificationContent()
+                    content.title = title
+                    content.body = message
+                    content.sound = UNNotificationSound.default
+                    
+                    let dateComp = Calendar.current.dateComponents([.year, .month, .month , .day, .hour, .minute], from: date)
+                    
+                    var trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: true)
+                    
+                    var request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    
+                    self.notifications.add(request) { (error) in
+                        if (error != nil){
+                            print("error" + error.debugDescription)
+                            self.okButtonLabel.isHidden = false
+                            return
+                        }
+                    }
+                    self.okButtonLabel.isHidden = false
+                    
+                }
+            }
+        }
+    }
+    
+    func second() {
+        let notifications = UNUserNotificationCenter.current()
+        
         notifications.getNotificationSettings { (settings) in
             DispatchQueue.main.async {
                 let title = self.titleTextField.text!
@@ -39,12 +96,13 @@ class ViewController: UIViewController {
                     let content = UNMutableNotificationContent()
                     content.title = title
                     content.body = message
+                    content.sound = UNNotificationSound.default
                     
                     let dateComp = Calendar.current.dateComponents([.year, .month, .month , .day, .hour, .minute], from: date)
                     
-                    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: false)
+                    var trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
                     
-                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    var request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
                     
                     self.notifications.add(request) { (error) in
                         if (error != nil){
@@ -52,36 +110,18 @@ class ViewController: UIViewController {
                             return
                         }
                     }
-                    let ac = UIAlertController(title: "Enable notifications", message: "To use this feature u must enable notifications in settings", preferredStyle: .alert)
-                    
-                    let goToSettings = UIAlertAction(title: "Settings", style: .default)
-                    { (_)in
-                        guard let settingsURL = URL(string: UIApplication.openSettingsURLString)
-                        else{
-                            return
-                        }
-                        if (UIApplication.shared.canOpenURL(settingsURL)){
-                            UIApplication.shared.open(settingsURL) { (_) in}
-                                
-                            }
-                        }
-                    ac.addAction(goToSettings)
-                    ac.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (_) in}))
-                    self.present(ac, animated: true)
                 }
             }
         }
-            }
-       
+    }
+    
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        self.notifications.removeAllPendingNotificationRequests()
+        self.okButtonLabel.text = "Stopped"
+
+    }
     
     
 }
-
-
-func formattedDate(date: Date) -> String{
-    let formatter = DateFormatter()
-    formatter.dateFormat = "d MMM y HH:mm"
-    return formatter.string(from: date)
-}
-
-
